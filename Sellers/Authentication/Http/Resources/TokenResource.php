786a81c6c6fcc344b57883d07c2e58513a7dd7f6
VitorNuness <vitor1nuness@gmail.com>
2024-10-25 20:55:53 -0300

    creating token resource to give json/array response

---
+++ Sellers/Authentication/Http/Resources/TokenResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Sellers\Authentication\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TokenResource extends JsonResource
+{
+    public function __construct(
+        public string $token,
+    ) {
+    }
+
+    public function toArray(Request $request): array
+    {
+        return [
+            "token" => $this->token,
+        ];
+    }
+}
