a5c5eab7ca066598c4ece359b6be012f4ff8599b
VitorNuness <vitor1nuness@gmail.com>
2024-10-25 21:14:18 -0300

    creating test to verify failure in the user registration and the request validation

--- tests/Feature/Http/Controllers/AuthenticationController/RegisterTest.php
+++ tests/Feature/Http/Controllers/AuthenticationController/RegisterTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\Controllers\AuthenticationController;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
@@ -29,5 +30,49 @@ class RegisterTest extends TestCase
             'email' => $newUser['email'],
         ]);
         $response->assertJsonPath('token', fn (?string $token) => strlen($token) > 0);
+    }
+
+    public function testCantRegisterANewUser(): void
+    {
+        // Act
+        $response = $this->postJson(route("auth.store"));
+
+        // Assert
+        $response->assertUnprocessable();
+        $this->assertDatabaseCount("users", 0);
+    }
+
+    public function testRequiredFields()
+    {
+        // Act
+        $response = $this->postJson(route("auth.store"));
+
+        // Assert
+        $response->assertUnprocessable();
+        $response->assertInvalid([
+            'name'     => 'required',
+            'email'    => 'required',
+            'password' => 'required',
+        ]);
+    }
+
+    public function testUniqueFields()
+    {
+        // Arrange
+        $user    = User::factory()->create();
+        $request = [
+            'name'     => fake()->name,
+            'email'    => $user->email,
+            'password' => fake()->password,
+        ];
+
+        // Act
+        $response = $this->postJson(route("auth.store"), $request);
+
+        // Assert
+        $response->assertUnprocessable();
+        $response->assertInvalid([
+            'email' => 'The email has already been taken.',
+        ]);
     }
 }
