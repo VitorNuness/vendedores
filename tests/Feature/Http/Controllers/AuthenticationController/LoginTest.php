a9838e944559db0a3627c51d930755c13ba8bc47
VitorNuness <vitor1nuness@gmail.com>
2024-10-26 08:48:26 -0300

    creating the invalid login tests and validate fields

--- tests/Feature/Http/Controllers/AuthenticationController/LoginTest.php
+++ tests/Feature/Http/Controllers/AuthenticationController/LoginTest.php
@@ -26,4 +26,64 @@ class LoginTest extends TestCase
         $response->assertOk();
         $response->assertJsonPath('token', fn (?string $token) => strlen($token) > 0);
     }
+
+    public function testCantLoginWithInvalidEmail(): void
+    {
+        // Arrange
+        $invalidCredentials = [
+            "email"    => fake()->email,
+            "password" => fake()->password,
+        ];
+
+        // Act
+        $response = $this->postJson(route("auth.login"), $invalidCredentials);
+
+        // Assert
+        $response->assertUnprocessable();
+    }
+
+    public function testCantLoginWithInvalidPassword(): void
+    {
+        // Arrange
+        $user          = User::factory()->create();
+        $wrongPassword = [
+            'email'    => $user->email,
+            'password' => 'wrong password',
+        ];
+
+        // Act
+        $response = $this->postJson(route("auth.login"), $wrongPassword);
+
+        // Assert
+        $response->assertUnauthorized();
+    }
+
+    public function testRequiredCredentials(): void
+    {
+        // Act
+        $response = $this->postJson(route("auth.login"));
+
+        // Assert
+        $response->assertInvalid([
+            'email'    => 'required',
+            'password' => 'required',
+        ]);
+    }
+
+    public function testEmailValidation(): void
+    {
+        // Arrange
+        $credentials = [
+            'email'    => 'aaa',
+            'password' => fake()->password(),
+        ];
+
+        // Act
+        $response = $this->postJson(route("auth.login"), $credentials);
+
+        // Assert
+        $response->assertInvalid([
+            'email' => 'valid',
+        ]);
+    }
 }
