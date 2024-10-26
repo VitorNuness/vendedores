4c0fd777036ea87f553909eec0496d802fe465b7
VitorNuness <vitor1nuness@gmail.com>
2024-10-25 22:11:30 -0300

    creating an "authenticable" request to apply authenticate method

---
+++ Sellers/Authentication/Http/Requests/AuthenticableRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Sellers\Authentication\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthenticableRequest extends FormRequest
+{
+    public function authenticate(): string
+    {
+        $credentials = [
+            'email'    => $this->input('email'),
+            'password' => $this->input('password'),
+        ];
+
+        return auth()->attempt($credentials);
+    }
+}
